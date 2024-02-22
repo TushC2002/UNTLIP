713ac6713c56ad0c24022bd40f8ea4c96b08ad40
Tushar Dilip Chaudhari <101420203+TushC2002@users.noreply.github.com>
2024-02-22 12:21:02 +0530

    sidebar nav-profile allorders

--- js/script.js
+++ js/script.js
@@ -1,12 +1,49 @@
+//Date
 function updateDate() {
     var currentDate = new Date();
     var options = { year: 'numeric', month: 'long', day: 'numeric' };
     var formattedDate = currentDate.toLocaleDateString('en-US', options);
     $('#current-date').text(formattedDate);
 }
-
 updateDate();
 
+//profiledropdown
+$(document).ready(function() {
+    
+    $(document).on('click', '#profile-toggle', function(event) {
+        event.stopPropagation();
+        $('#profile-dropdown').toggle();
+    });
+
+    
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('#profile-dropdown').length) {
+            $('#profile-dropdown').hide();
+        }
+    });
+
+    
+    $('#view-profile').click(function() {
+        // Logic 
+        alert('View Profile clicked');
+    });
+
+    
+    $('#logout').click(function() {
+        // Logic
+        alert('Logout clicked');
+    });
+});
+
+//daily&monthly toggle
+$(document).ready(function(){
+    $('.toggle-btn').click(function(){
+        $('.toggle-btn').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+//sidebar
 function loadContent(url, containerId) {
     $.get(url, function(data) {
         $(`#${containerId}`).html(data);
@@ -36,18 +73,21 @@ $(document).ready(function() {
             firstLink.addClass('active');
         }
     }
-
-    var currentUrl = window.location.href;
-    if (currentUrl.indexOf('dashboard.html') === -1) {
-        loadContent('dashboard.html', 'dynamicContent');
-        $('.menu a[href="dashboard.html"]').addClass('active');
-    }
 });
 
 
-
-
-
-
-
-
+//orderstabs
+$(document).ready(function() {
+    var containerLinks = $('.nav a');
+    containerLinks.each(function() {
+        $(this).on('click', function(event) {
+            event.preventDefault();
+            var href = $(this).attr('href');
+            var targetId = $(this).attr('data-target');
+            loadContent(href, targetId);
+            
+            containerLinks.removeClass('active');
+            $(this).addClass('active');
+        });
+    });
+});
